e86f7ab1ed94149c73c3fbbca9324d7a2550d8ed
Atir Nayab <91016903+nayabatir1@users.noreply.github.com>
2023-02-19 02:43:58 +0530

    remove console logs from amazon and flipkart parser (#12)
    persisted product data

--- src/store/Store.tsx
+++ src/store/Store.tsx
@@ -1,15 +1,22 @@
 import {create} from 'zustand';
+import {persist, createJSONStorage} from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import {Store} from '../types/store';
 
-const useStore = create<Store>(set => ({
-  products: [],
-  addProducts: product =>
-    set(state => ({products: [...state.products, product]})),
-  removeProduct: id =>
-    set(state => ({
-      products: state.products.filter(product => product.id !== id),
-    })),
-}));
+const useStore = create<Store, [['zustand/persist', unknown]]>(
+  persist(
+    (set, _get) => ({
+      products: [],
+      addProducts: product =>
+        set(state => ({products: [...state.products, product]})),
+      removeProduct: id =>
+        set(state => ({
+          products: state.products.filter(product => product.id !== id),
+        })),
+    }),
+    {name: 'frugally', storage: createJSONStorage(() => AsyncStorage)},
+  ),
+);
 
 export default useStore;
