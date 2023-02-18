cc2065251ce201a566e3ece8c94723db4a74af44
Atir Nayab <nayabatir1@gmail.com>
2023-02-19 01:48:32 +0530

    amazon link cleanup memoized style for product card

--- src/utils/ParseAmazonLink.ts
+++ src/utils/ParseAmazonLink.ts
@@ -3,11 +3,8 @@ import he from 'he';
 import {Product} from '../types/product';
 
 async function ParseAmazonLink(link: string): Promise<Product | void> {
-  console.log(link);
   try {
     const res = await fetch(link);
-
-    console.log(res.status);
 
     let text = await res.text();
 
@@ -37,8 +34,6 @@ function extractImage(html: string) {
 
   const [img1] = html.match(exp1) || [''];
 
-  console.log(img1);
-
   const exp2 = new RegExp(/src=".+?"/gm);
 
   const [img2] = img1.match(exp2) || [''];
@@ -65,15 +60,11 @@ function extractPrice(html: string) {
   const [currencyDiv] = html.match(currencyDivExp) || [''];
   const [priceDiv] = html.match(priceDivExp) || [''];
 
-  console.log(priceDiv);
-
   const currencyExp = new RegExp(/>\S+?</gm);
   const priceExp = new RegExp(/>\S+?</gm);
 
   const currency = currencyDiv.match(currencyExp)?.[0].slice(1, -1) || '';
   const price = priceDiv.match(priceExp)?.[0].slice(1, -1) || '';
-
-  console.log({currency, price});
 
   return currency + price;
 }
